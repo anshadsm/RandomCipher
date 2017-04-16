590dc39089662bac5b15b7521bf0cde807c82a95
Anshad S <anshadmahamood@gmail.com>
2017-04-16 22:37:50 +0530

    Create common.py

---
+++ common.py
@@ -0,0 +1,29 @@
+from Crypto.Cipher import AES
+from datetime import datetime
+
+key = '1234567891234567'
+message = 'your_message'+" "+str(datetime.now())
+len_time = len(str(datetime.now()))
+cipher = AES.new(key)
+
+def pad(s):
+    return s + ((16-len(s)%16) * '{')
+
+def encrypt(plaintext):
+    global cipher
+    return cipher.encrypt(plaintext)
+
+def decrypt(ciphertext):
+    global cipher
+    dec = cipher.decrypt(ciphertext)
+    l= dec.count('{')
+    return dec[:len(dec)-l-(len_time+1)]
+
+
+print("message :", message)
+message=pad(message)
+encrypted = encrypt(message)
+decrypted = decrypt(encrypted)
+print("Encrypted :",encrypted)
+print("Decrypted :",decrypted)
+
